3c9b525865cf3061881794df131d1f76c4e237f0
Samuel <spage930@gmail.com>
2022-07-11 22:01:20 -0400

    complete side deck array

--- src/sideDeck.js
+++ src/sideDeck.js
@@ -1,7 +1,50 @@
 export const sideDeck = [
     {
         id: 1,
+        value: -1,
+    },
+    {
+        id: 2,
+        value: -2,
+    },
+    {
+        id: 3,
+        value: -3,
+    },
+    {
+        id: 4,
+        value: -4,
+    },
+    {
+        id: 5,
+        value: -5,
+    },
+    {
+        id: 6,
+        value: -6,
+    },
+    {
+        id: 7,
         value: 1,
-        status: positive
+    },
+    {
+        id: 8,
+        value: 2,
+    },
+    {
+        id: 9,
+        value: 3,
+    },
+    {
+        id: 10,
+        value: 4,
+    },
+    {
+        id: 11,
+        value: 5,
+    },
+    {
+        id: 12,
+        value: 6,
     },
 ]
